3a3c0a824383f5c1e5f500be02ce03d7a5067c19
abhishekrai4 <49629614+abhishekrai4@users.noreply.github.com>
2019-04-18 04:26:42 -0700

    Add files via upload

---
+++ Ide.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+
+        int a;
+        scanf("%d",&a);
+        if (a==0)
+        printf("Zero");
+        else if(a>0)
+        printf("Positive");
+        else
+        printf("Negetive");
+
+
+	return 0;
+}
+
